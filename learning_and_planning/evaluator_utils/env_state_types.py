b76525fc90b28d0bf2037c4ef239237a22f14b36
learningandplanningICLR <learningandplanning@gmail.com>
2019-11-12 12:45:36 +0000

    Remove some unused code.

--- learning_and_planning/evaluator_utils/env_state_types.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class EnvState(Enum):
-  # The mapping to integers IS important and used in many places.
-  DEAD = 0  # Dead-end, level cannot be solved.
-  SOLVED = 1  # Level is solved.
-  SOLVABLE = 2  # Level is neither solved or in dead-end.
